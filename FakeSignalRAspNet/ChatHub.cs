4df86e35e6a0d6f2bcbdab0789a54a5d6e440e74
Gabe Shapiro <gabeshap@outlook.com>
2019-08-20 22:54:38 -0700

    first commit

---
+++ FakeSignalRAspNet/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+//https://docs.microsoft.com/en-us/aspnet/signalr/overview/getting-started/tutorial-getting-started-with-signalr
+//https://docs.microsoft.com/en-us/aspnet/signalr/overview/performance/scaleout-with-redis
+//https://docs.microsoft.com/en-us/aspnet/signalr/overview/deployment/using-signalr-with-azure-web-sites
+
+namespace FakeSignalRAspNet
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            try
+            {
+                System.Diagnostics.Trace.WriteLine("before - tracing instance for: " + name + " " + message + " " + Environment.GetEnvironmentVariable("WEBSITE_INSTANCE_ID"));
+                // Call the broadcastMessage method to update clients.
+                Clients.All.broadcastMessage(name, message);
+                System.Diagnostics.Trace.WriteLine("after - tracing instance for: " + name + " " + message + " " + Environment.GetEnvironmentVariable("WEBSITE_INSTANCE_ID"));
+            }
+
+            catch (Exception ex)
+            {
+                System.Diagnostics.Trace.WriteLine("exception occurred during chathub on instance: " + Environment.GetEnvironmentVariable("WEBSITE_INSTANCE_ID") + " " +ex.ToString());
+
+            }
+        }
+    }
+}
---
+++ FakeSignalRAspNet/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+//using Microsoft.AspNet.SignalR.StackExchangeRedis;
+using Microsoft.AspNet.SignalR.Redis;
+using Microsoft.AspNet.SignalR;
+
+//https://docs.microsoft.com/en-us/aspnet/signalr/overview/performance/scaleout-with-redis
+
+//https://stackoverflow.com/questions/35325531/signalr-redis-backplane-not-working-dependency-issue
+
+
+[assembly: OwinStartup(typeof(FakeSignalRAspNet.Startup))]
+
+//https://github.com/aspnet/AzureSignalR-samples/tree/master/aspnet-samples/ChatRoom
+
+namespace FakeSignalRAspNet
+{
+    public class Startup
+    {
+        private string connectionString = "MyFakeRedisNamespace.redis.cache.windows.net:6380,password=MyFakeRedispassword=,ssl=True,abortConnect=False";
+        private string eventKey = "FakeSignalRAspNet";
+
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            try
+            {
+                GlobalHost.DependencyResolver.UseRedis(new RedisScaleoutConfiguration(connectionString, eventKey));
+                app.MapSignalR();
+                System.Diagnostics.Trace.WriteLine("connected to redis");
+
+            }
+            catch(Exception ex)
+            {
+                System.Diagnostics.Trace.WriteLine("exception occurred: " + ex.ToString());
+            }
+        }
+    }
+}
